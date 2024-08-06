3e7ad1af4a01b0ee64f23e1d6972b5556a2486ad
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-08-06 16:50:21 -0300

    criacao de url dinamicas e dinamizando o codigo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 
 class Jogo:
@@ -22,7 +22,10 @@ def index():
 
 @app.route('/new')
 def new():
-    return render_template('add_game.html', titulo = 'Novo Jogo')
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('new')))
+    else:
+        return render_template('add_game.html', titulo = 'Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
 def criar():
@@ -32,27 +35,29 @@ def criar():
     preco = request.form['preco']
     jogo = Jogo(nome, categoria, console, preco)
     lista_jogo.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
     if 'admin' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(session['usuario_logado'] + ' '+ '  logado com sucesso')
-        return redirect('/')
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
     else:
         flash('Usuário não logado')
-        return redirect('/login')
+        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso')
-    return redirect('/login')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
