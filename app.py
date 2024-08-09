4fc9597abe3f89616e29c7a1ec98fd1fe6bb4ed3
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-08-08 21:28:27 -0300

    criacao de classe usuario para conexao com banco

--- app.py
+++ app.py
@@ -44,6 +44,24 @@ app.config('SQLALCHEMY_DATABASE_URI') = \
 
 db = SQLAlchemy(app)
 
+class Jogos(db.Model):
+    id=db.Column(db.Integer, primary_key=True,autoincrement=True)
+    nome= db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(40), nullable=False)
+    console = db.Column(db.String(20), nullable=False)
+
+    #CRIANDO FUNCAO DE INICIACAO
+    def __repr__(self):
+        return '<Name %r>'% self.name
+    
+class Usuarios(db.Model):
+    nickname=db.Column(db.String(8), primary_key=True,)
+    nome= db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+
+    #CRIANDO FUNCAO DE INICIACAO
+    def __repr__(self):
+        return '<Name %r>'% self.name
 
 
 @app.route('/')
