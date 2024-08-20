aae8b62fdb492b080cfcff50174a469a8e15d7af
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-08-20 15:35:19 -0300

    adicionando o csrf para melhor seguranca da aplicacao

--- app.py
+++ app.py
@@ -2,11 +2,12 @@
 #PARA INICIAR O AMBIENTE VIRTUAL.venv\Scripts\activate 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 #INSTANCIANDO O DB 
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 from views import *
 if __name__ == "__main__":
     app.run(debug=True)
