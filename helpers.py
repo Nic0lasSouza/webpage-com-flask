2e565149dd89549ce93521659f0205b83dcad98a
nicolas souza <nicolasdsmdsouza2004@gmail.com>
2024-08-20 14:54:26 -0300

    atualizando os arquivos pois havia erros de importacao

--- helpers.py
+++ helpers.py
@@ -4,9 +4,9 @@ from wtforms import StringField, validators, SubmitField
 from app import app
 
 class FormularioJogo(FlaskForm):
-    nome = StringField('nome', [validators.DataRequired(), validators.length(min=1, max=50)])
-    categoria = StringField('categoria', [validators.DataRequired(), validators.length(min=1, max=40)])
-    console = StringField('console', [validators.DataRequired(), validators.length(min=1, max=20)])
+    nome = StringField('Nome do jogo:', [validators.DataRequired(), validators.length(min=1, max=50)])
+    categoria = StringField('Categoria:', [validators.DataRequired(), validators.length(min=1, max=40)])
+    console = StringField('Console:', [validators.DataRequired(), validators.length(min=1, max=20)])
     salvar = SubmitField('salvar')
 def recupera_imagem(id):
     for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
 from app import app, db
 from models import Jogos,Usuarios
-from helpers import recupera_imagem
+from helpers import recupera_imagem,deleta_arquivo, FormularioJogo
 import time
 
 @app.route('/')
@@ -50,24 +50,32 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('edit')))
     jogo =Jogos.query.filter_by(id=id).first()
+    form = FormularioJogo()
+    form.nome.data = jogo.nome
+    form.categoria.data = jogo.categoria
+    form.console.data = jogo.console
+    
     capa_jogo = recupera_imagem(id)
-    return render_template('editar.html', titulo = 'Editando jogo', jogo=jogo, capa_jogo=capa_jogo)
+    return render_template('editar.html', titulo = 'Editando jogo', id=id, capa_jogo=capa_jogo, form =form)
 
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
-    jogo =Jogos.query.filter_by(id=request.form['id']).first()
-    jogo.nome = request.form['nome']
-    jogo.categoria = request.form['categoria']
-    jogo.console = request.form['console']
+    form = FormularioJogo(request.form)
+    if form.validate_on_submit():
+        jogo =Jogos.query.filter_by(id=request.form['id']).first()
+        jogo.nome = form.nome.data
+        jogo.categoria = form.categoria.data
+        jogo.console = form.console.data
 
-    db.session.add(jogo)
-    db.session.commit()
+        db.session.add(jogo)
+        db.session.commit()
 
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    flash('Jogo editado com sucesso')
+        arquivo = request.files['arquivo']
+        upload_path = app.config['UPLOAD_PATH']
+        timestamp = time.time()
+        deleta_arquivo(jogo.id)
+        arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+        flash('Jogo editado com sucesso')
     return redirect(url_for('index'))
 
 @app.route('/deletar<int:id>')
